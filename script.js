717d31be0c982bff78159a1358a21a6037e9cc6e
laupwing <laupwing@gmail.com>
2025-02-16 17:17:17 +0100

    style: add hover effect for gallery images and implement scroll class toggle

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 const images = document.querySelectorAll("#gallery img")
 
-console.log(images)
+images.forEach((image) => {
+    const reset = () => {
+        images.forEach((image) => {
+            image.style.opacity = 1
+        })
+    }
+
+    const changeOpacity = () => {
+        images.forEach((image) => {
+            image.style.opacity = 0.7
+        })
+    }
+
+    image.addEventListener("mouseover", () => {
+        changeOpacity()
+        image.style.opacity = 1
+    })
+
+    image.addEventListener("mouseout", () => {
+        reset()
+    })
+})
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+        document.body.classList.add("scrolled")
+    } else {
+        document.body.classList.remove("scrolled")
+    }
+})
